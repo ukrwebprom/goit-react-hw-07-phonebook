e0f1b4dbbac71504f1fa657373916b00b010f624
ukrwebprom <ukrwebprom@gmail.com>
2023-04-03 20:48:41 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './filterSlice';
 import { contactReducer } from './contactSlice';
-import storage from 'redux-persist/lib/storage';
-import { createAction } from "@reduxjs/toolkit";
-
 
 
 export const store = configureStore({
